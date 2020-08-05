4b02602b609bb15d5cbe11340ef7282a07f2e465
Kitty Jose <kitty@datachemist.com>
2020-08-05 13:10:16 +0100

    making electron work with new https console

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -8,26 +9,54 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3005' : `file://${path.join(__dirname, '../build/index.html')}`);
-  if (isDev) {
-    // Open the DevTools.
-    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-    mainWindow.webContents.openDevTools();
-  }
-  mainWindow.on('closed', () => mainWindow = null);
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 680,
+        nodeIntegration: false,
+        webPreferences: {
+            plugins: true,
+            webSecurity: false,
+            allowDisplayingInsecureContent: true,
+            allowRunningInsecureContent: true
+        }
+    });
+
+    app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+    const options = { extraHeaders: 'pragma: no-cache\n' }
+    mainWindow.loadURL('http://localhost:3005', options)
+    //mainWindow.loadURL(isDev ? 'http://localhost:3005' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+    if (isDev) {
+        // Open the DevTools.
+        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+        mainWindow.webContents.openDevTools();
+        app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+            //console.log(' certificate error url', url)
+            callback(true)
+            mainWindow.webContents.openDevTools();
+
+            mainWindow.webContents.on("devtools-opened", () => { mainWindow.webContents.closeDevTools(); });
+        })
+    }
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    });
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+    createWindow()
+})
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', (url) => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+
+app.on('activate', (url) => {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
