3c69c7893e0141d4e40276a2492aa3b5c7c92c52
Dmytri Kleiner <dk@trick.ca>
2020-09-03 17:58:22 +0200

    use stderr to respond to sterver start

--- public/electron.js
+++ public/electron.js
@@ -1,128 +1,129 @@
-const electron = require('electron');
+const electron = require('electron')
 const path = require('path')
 const fs = require('fs')
+const execFile = require('child_process').execFile
 
-const app = electron.app;
-const Menu = electron.Menu;
-const Tray = electron.Tray;
+const isDev = require('electron-is-dev')
 
-const BrowserWindow = electron.BrowserWindow;
+let mainWindow
+let tray = null
+const clippings = ['TerminusDB']
 
-const isDev = require('electron-is-dev');
+function createWindow () {
+  mainWindow = new electron.BrowserWindow({
+    width: 900,
+    height: 680,
+    nodeIntegration: false,
+    webPreferences: {
+      plugins: true,
+      webSecurity: false,
+      allowDisplayingInsecureContent: true,
+      allowRunningInsecureContent: true
+    },
+    icon: path.join(__dirname, 'assets/icons/favicon.png')
+  })
 
-const appDir = path.dirname(require.main.filename);
-const execFile = require('child_process').execFile;
+  // const options = { extraHeaders: 'pragma: no-cache\n' }
+  mainWindow.setMenu(null)
 
+  if (isDev) {
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
 
-let mainWindow;
-let tray = null;
-const clippings = ['TerminusDB'];
+    mainWindow.on('minimize', function (event) {
+      event.preventDefault()
+      mainWindow.hide()
+    })
 
-function startTerminusDB(callback) {
-    const appImagePath = `${appDir}/TerminusDB.AppImage`
-    const exePath = `${appDir}/windows/start_windows.bat`
-    if (fs.existsSync(appImagePath)) {
-        execFile(appImagePath, ['serve'], (error, stdout, stderr) => {
-            console.log(error)
-            console.log(stdout)
-            console.log(stderr)
-        })
-    }
-    else if (fs.existsSync(exePath)) {
-        execFile(exePath, [], (error, stdout, stderr) => {
-            console.log(error)
-            console.log(stdout)
-            console.log(stderr)
-        })
-    }
-}
-
-function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 900,
-        height: 680,
-        nodeIntegration: false,
-        webPreferences: {
-            plugins: true,
-            webSecurity: false,
-            allowDisplayingInsecureContent: true,
-            allowRunningInsecureContent: true
-        },
-        icon: path.join(__dirname, 'assets/icons/favicon.png')
-    });
-
-    const options = { extraHeaders: 'pragma: no-cache\n' }
-    startTerminusDB();
-    setTimeout(() => {
-        mainWindow.loadURL('https://127.0.0.1:6363/')
-    }, 8000);
-    console.log("Terminusdb started");
-    mainWindow.setMenu(null)
-
-    //mainWindow.loadURL(isDev ? 'http://localhost:3005' : `file://${path.join(__dirname, '../build/index.html')}`);
-
-    if (isDev) {
-        // Open the DevTools.
-        //mainWindow.webContents.openDevTools();
-
-
-        mainWindow.on('minimize',function(event){
-            event.preventDefault();
-            mainWindow.hide();
-        });
-
-        mainWindow.on('close', function (event) {
-
-            if(!app.isQuiting){
-                event.preventDefault();
-                mainWindow.hide();
-            }
-            return false;
-        });
-    }
-
-    electron.globalShortcut.register('f5', () => mainWindow.reload())
-    electron.globalShortcut.register('CommandOrControl+R', () => mainWindow.reload())
-}
-
-app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
-    if (url.startsWith('https://127.0.0.1')) {
-        callback(true)
-    }
-    else {
-        callback(false)
-    }
-    //mainWindow.webContents.openDevTools();
-    mainWindow.webContents.on("devtools-opened", () => { mainWindow.webContents.closeDevTools(); });
-})
-
-app.on('ready', () => {
-  if (app.dock) app.dock.hide();
-
-  tray = new Tray(path.join(__dirname, 'assets/icons/favicon.png'));
-  if (process.platform === 'win32') {
-    tray.on('click', tray.popUpContextMenu);
+    mainWindow.on('close', function (event) {
+      if (!electron.app.isQuiting) {
+        event.preventDefault()
+        mainWindow.hide()
+      }
+      return false
+    })
   }
 
+  electron.globalShortcut.register('f5', () => mainWindow.reload())
+  electron.globalShortcut.register('CommandOrControl+R', () => mainWindow.reload())
 
-  createMenu();
-  tray.setToolTip('TerminusDB');
-  createWindow();
-});
+  console.log('ready')
+  mainWindow.loadURL('https://127.0.0.1:6363/')
+}
+
+electron.app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+  if (url.startsWith('https://127.0.0.1')) {
+    callback(true)
+  } else {
+    callback(false)
+  }
+  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.on('devtools-opened', () => { mainWindow.webContents.closeDevTools() })
+})
+
+electron.app.on('ready', () => {
+  const startConsole = () => {
+    if (electron.app.dock) electron.app.dock.hide()
+
+    tray = new electron.Tray(path.join(__dirname, 'assets/icons/favicon.png'))
+    if (process.platform === 'win32') {
+      tray.on('click', tray.popUpContextMenu)
+    }
+
+    createMenu()
+    tray.setToolTip('TerminusDB')
+    createWindow()
+  }
+
+  const appDir = path.dirname(require.main.filename)
+  const appImagePath = `${appDir}/TerminusDB.AppImage`
+  const exePath = `${appDir}/windows/start_windows.bat`
+
+  let binPath
+  let binArgs
+  if (fs.existsSync(appImagePath)) {
+    binPath = appImagePath
+    binArgs = ['serve']
+  } else if (fs.existsSync(exePath)) {
+    binPath = exePath
+    binArgs = []
+  } else {
+    console.log('Terminusdb appImage Not Found')
+  }
+
+  console.log('PATH', binPath)
+  let serverReady = false
+  console.log('Starting TerminusDB')
+  const child = execFile(binPath, binArgs)
+  child.stderr.on('data', (data) => {
+    if (!serverReady) {
+      console.log('stderr: ' + data)
+      if (data.toString().substring(2, 16) === 'Started server') {
+        serverReady = true
+        console.log('Terminusdb started')
+        startConsole()
+      }
+    }
+  })
+})
 
 const createMenu = () => {
-  const menu = Menu.buildFromTemplate([
-    ...clippings.map((clipping, index) => ({ label: clipping, click() {
-        mainWindow.show();
-      }})),
+  const menu = electron.Menu.buildFromTemplate([
+    ...clippings.map((clipping, index) => ({
+      label: clipping,
+      click () {
+        mainWindow.show()
+      }
+    })),
     { type: 'separator' },
     {
       label: 'Quit',
-      click() {
-        app.isQuiting = true;
-        app.quit(); },
+      click () {
+        electron.app.isQuiting = true
+        electron.app.quit()
+      }
     }
-  ]);
+  ])
 
-  tray.setContextMenu(menu);
-};
+  tray.setContextMenu(menu)
+}
